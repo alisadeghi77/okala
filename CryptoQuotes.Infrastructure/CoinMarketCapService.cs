252dec35bdde0037f8e5847bc8b53a57ed871898
sadeghi.alii <alisadeghi431@gmail.com>
2025-03-15 04:37:19 +0330

    feature(infrastructure): implement coin market cap service

---
+++ CryptoQuotes.Infrastructure/CoinMarketCapService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+using CryptoQuotes.Core;
+
+namespace CryptoQuotes.Infrastructure;
+
+public class CoinMarketCapService(HttpClient httpClient) : ICoinMarketCapService
+{
+    private const string StaticEndPoint = "v2/cryptocurrency/quotes/latest?symbol={0}&convert={1}";
+
+    public async Task<Result<CoinMarketCapResponse>> GetCryptoLatestQuotesAsync(
+        string cryptoCode,
+        string baseCurrency,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var endPoint = string.Format(StaticEndPoint, cryptoCode, baseCurrency);
+
+            var response = await httpClient.GetAsync(endPoint, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+                return Result<CoinMarketCapResponse>.Failure(
+                    new Error(response.StatusCode.ToString(), $"status code: {response.StatusCode}")
+                        .AddData(await response.Content.ReadAsStringAsync(cancellationToken)));
+
+            var result = await response.Content.ReadFromJsonAsync<CoinMarketCapResponse>(cancellationToken);
+            
+            if (result is null)
+            {
+                var exception = new Exception("cryptocurrency/quotes/latest response is null");
+                exception.Data.Add("response", await response.Content.ReadAsStringAsync(cancellationToken));
+                throw exception;
+            }
+            
+            return Result<CoinMarketCapResponse>.Success(result);
+        }
+        catch (HttpRequestException ex)
+        {
+            return Result<CoinMarketCapResponse>.Failure(new Error("HTTP_ERROR", ex.Message).AddData(ex));
+        }
+    }
+}
---
+++ CryptoQuotes.Infrastructure/ExchangeRatesService.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Json;
+using CryptoQuotes.Core;
+using Microsoft.Extensions.Options;
+
+namespace CryptoQuotes.Infrastructure;
+
+public class ExchangeRatesService(
+    HttpClient httpClient,
+    IOptions<ExchangeRatesSettings> exchangeRatesSettings)
+    : IExchangeRatesService
+{
+    private readonly ExchangeRatesSettings _exchangeRatesSettings = exchangeRatesSettings.Value;
+
+    private const string StaticEndPoint = "latest?access_key={0}&symbols={1}&base={2}";
+
+    public async Task<Result<ExchangeRatesResponse>> GetExchangeRatesAsync(
+        string baseCurrency,
+        IEnumerable<string> symbols,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var endPoint = string.Format(StaticEndPoint, _exchangeRatesSettings.ApiKey, string.Join(',', symbols), baseCurrency);
+
+            var response = await httpClient.GetAsync(endPoint, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+                return Result<ExchangeRatesResponse>.Failure(
+                    new Error(response.StatusCode.ToString(), $"status code: {response.StatusCode}")
+                        .AddData(await response.Content.ReadAsStringAsync(cancellationToken)));
+
+            var result = await response.Content.ReadFromJsonAsync<ExchangeRatesResponse>(cancellationToken);
+
+            if (result is null)
+            {
+                var exception = new Exception("cryptocurrency/quotes/latest response is null");
+                exception.Data.Add("response", await response.Content.ReadAsStringAsync(cancellationToken));
+                throw exception;
+            }
+
+            return Result<ExchangeRatesResponse>.Success(result);
+        }
+        catch (HttpRequestException ex)
+        {
+            return Result<ExchangeRatesResponse>.Failure(new Error("HTTP_ERROR", ex.Message).AddData(ex));
+        }
+    }
+}
