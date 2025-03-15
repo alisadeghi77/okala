95931e849dbbd8af9192ed887f4a0b509c908324
sadeghi.alii <alisadeghi431@gmail.com>
2025-03-15 04:39:39 +0330

    feature(api): implement exception middle ware

---
+++ CryptoQuotes.WebApi/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace CryptoQuotes.WebApi;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An unhandled exception occurred.");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var response = new
+        {
+            StatusCode = (int)HttpStatusCode.InternalServerError,
+            Message = "An unexpected error occurred. Please try again later."
+        };
+
+        var jsonResponse = JsonSerializer.Serialize(response);
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        return context.Response.WriteAsync(jsonResponse);
+    }
+}
