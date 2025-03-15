3e956fb0727686f3ed5bed51a910416766eb3c83
sadeghi.alii <alisadeghi431@gmail.com>
2025-03-15 04:39:17 +0330

    feature(api): implement controller, api and call query

---
+++ CryptoQuotes.WebApi/CryptoController.cs
@@ -0,0 +1,23 @@
+﻿using CryptoQuotes.Application;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CryptoController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CryptoController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("{cryptoSymbol}")]
+    public async Task<IActionResult> GetCryptoQuote(string cryptoSymbol)
+    {
+        var result = await _mediator.Send(new GetCryptoQuoteQuery(cryptoSymbol));
+        
+        return result.IsSuccess ? Ok(result) : Problem(result.Error.Message);
+    }
+}
